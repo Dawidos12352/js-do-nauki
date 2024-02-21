9a493164567079e46f5da0731631701c5b93ca7f
Dawid Wrobel <118534055+Dawidos12352@users.noreply.github.com>
2024-02-21 06:50:29 +0100

    update

---
+++ REST API infinite scroll/scripts/config.js
@@ -0,0 +1,10 @@
+export const API_PATH = 'https://pixabay.com/api';
+export const API_KEY = '35988919-7ec9329d85026b7b4e8ec28c4';
+
+export const DEFAULT_PIXABAY_PARAMS = {
+  key: API_KEY,
+  per_page: '40',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: 'true',
+};
