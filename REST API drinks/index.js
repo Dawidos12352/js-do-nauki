5a86cbd2d30bcb173ec456c5ccc2d98ea646c2e5
Dawid Wrobel <118534055+Dawidos12352@users.noreply.github.com>
2024-02-23 10:28:55 +0100

    added styles and ingredients

--- REST API drinks/index.js
+++ REST API drinks/index.js
@@ -1,4 +1,5 @@
 const API_PATH = 'https://www.thecocktaildb.com/api/json/v1/1/search.php'
+const POSSIBLE_INGREDIENTS_AMOUNT = 15;
 
 const form = document.querySelector("form")
 const drinksRecipes = document.querySelector("#drinks")
@@ -19,15 +20,40 @@ respsnse.then((data) => {
        data.json()
        .then(({drinks}) => {
         console.log(drinks)
+
         drinksRecipes.innerHTML = "";
         drinks.forEach((el) => {
-            console.log()
+
+            const ingridientsArray =[]
+            for (let i = 1; i <= POSSIBLE_INGREDIENTS_AMOUNT; i++){
+               console.log(`${el[`strIngredient${i}`]}`)
+
+               const ing = el[`strIngredient${i}`]
+               const mes = el[`strMeasure${i}`]
+               if(ing || mes) {
+                ingridientsArray.push(`${mes || ""} - ${ing || ""}`)
+               }
+               
+            }
+            console.log(ingridientsArray)
+
+            
+            const listRecipe = el.strInstructions.split(". ").map((e) => `<li>${e}</li>`).join("")
+
             drinksRecipes.insertAdjacentHTML("beforeend" , `
                 <div>
                     <h2 class="drink-title">${el.strDrink}</h2>
                     <div class="drink-box">
                         <img src="${el.strDrinkThumb}" alt="${el.strDrink}" width="600px" class="image" />
-                        <p class="description">${el.strInstructions}</p>
+                        <div>
+                            <h3>Ingredients</h3>
+                            <ul class="description">${ingridientsArray.map((e) => 
+                                `<li>${e}</li>`
+                            ).join(" ")
+                        }</ul>
+                            <h3>Preparation</h3>
+                            <ol class="description">${listRecipe}</ol>
+                        <div>
                     </div>
                 </div>
             `)
@@ -38,5 +64,9 @@ respsnse.then((data) => {
 .catch((e) => {
     console.log(e)
 })
+
+
+
+
 
 
